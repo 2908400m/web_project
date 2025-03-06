9b5f0bb7ba05cffc0b13a72e719c50951f58fdfe
Connor Mackenzie <2885877M@student.gla.ac.uk>
2025-03-06 17:29:39 +0000

    models

--- drop_the_beat/admin.py
+++ drop_the_beat/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Artist, Genre, Song, UserProfile, Review
 
-# Register your models here.
+admin.site.register(Artist)
+
+admin.site.register(Genre)
+
+admin.site.register(Song)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'favorite_genre')
+    search_fields = ('user__username', 'user__email', 'bio')
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'song', 'rating', 'comment')
+    search_fields = ('user__user__username', 'song__title', 'comment')
+
+admin.site.register(Review, ReviewAdmin)
--- drop_the_beat/models.py
+++ drop_the_beat/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bio = models.TextField(blank = True)
+    image = models.ImageField(upload_to="artist_image/", blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    genre = models.CharField(choices=[('pop', 'Pop'), ('rock', 'Rock'), ('rap', 'Rap')])
+
+    def __str__(self):
+        return self.genre
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="songs")
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name="genre_songs")
+
+    def __str__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.textfield(blank=True)
+    profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True)
+    favorite_genre = models.ForeignKey(Genre, null=True, blank=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.user.username
+    
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.PositiveIntegerField(choices=[1,2,3,4,5], default=5)  
+    comment = models.TextField(max_length=255, blank = True)
+
+    class Meta:
+        unique_together = ("user", "song")
+
+    def __str__(self):
+        return f"Review by {self.user.name} for {self.song.title}"
